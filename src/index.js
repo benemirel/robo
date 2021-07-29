e403054d3c8a54192c329c4438220527c67d1420
Mirel Bene <mirelbene@gmail.com>
2021-07-29 19:55:05 +0300

    first change

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<h1>Hello World</h1>,
   document.getElementById('root')
 );
 
