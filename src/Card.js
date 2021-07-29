1cf702c240e6ca617ba32e2b58ddf11e9794e815
Mirel Bene <mirelbene@gmail.com>
2021-07-29 21:13:55 +0300

    Deleted app file imported tachyons created card file

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () =>{
+return(
+<div>
+<img alt ='robocard' src='https://robohash.org/wow' />
+<div>
+<h2>Jane Doe</h2>
+<p>janedoe@whatever.com</p>
+</div>
+</div>
+
+);
+}
+export default Card;
